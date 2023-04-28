f355d8d471c08119a167416eeff7e316678f7380
Mike Repec <mikerepec@gmail.com>
2023-04-28 22:48:58 +0200

    Remove test log

--- bot.py
+++ bot.py
@@ -123,8 +123,6 @@ def debug_print(msg: str):
     if os.getenv("DEBUG") == "true":
         print(f"[DEBUG] {msg}")
 
-print("test log")
-
 if __name__ == "__main__":
     try:
         load_dotenv()
